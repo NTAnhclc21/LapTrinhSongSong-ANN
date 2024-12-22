710b8349c0073e925c28506415a38dd2d53d9dd7
DangDecTen <100565626+DangDecTen@users.noreply.github.com>
2024-12-22 11:01:33 +0700

    Add bin folder

--- include/hyperparameters.h
+++ include/hyperparameters.h
@@ -2,10 +2,10 @@
 #define HYPERPARAMETERS_H
 
 // MNIST-fashion file path
-#define TRAIN_IMAGES    "train-images-idx3-ubyte"
-#define TRAIN_LABELS    "train-labels-idx1-ubyte"
-#define TEST_IMAGES     "t10k-images-idx3-ubyte"
-#define TEST_LABELS     "t10k-labels-idx1-ubyte"
+#define TRAIN_IMAGES    "data/train-images-idx3-ubyte"
+#define TRAIN_LABELS    "data/train-labels-idx1-ubyte"
+#define TEST_IMAGES     "data/t10k-images-idx3-ubyte"
+#define TEST_LABELS     "data/t10k-labels-idx1-ubyte"
 
 // Hyperparameter definitions
 #define INPUT_LAYER_SIZE      784       // MNIST-fashionfashion 28x28 image
